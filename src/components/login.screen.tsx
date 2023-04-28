2fa2a7143c88df615f04192094fb7f2dee2c190a
HieuVu <hieu.vh301195@gmail.com>
2023-04-28 19:35:23 +0700

    init project

---
+++ src/components/login.screen.tsx
@@ -0,0 +1,20 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+
+const LoginScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default LoginScreen;
---
+++ src/navigation/type.navigation.tsx
@@ -0,0 +1,14 @@
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RouteProp} from '@react-navigation/native';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Game: undefined;
+  Login: undefined;
+  Splash: undefined;
+};
+
+export type MessageNavigationProps<T extends keyof RootStackParamList> = {
+  navigation: NativeStackNavigationProp<RootStackParamList, T>;
+  route: RouteProp<RootStackParamList, T>;
+};
